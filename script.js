103c33866a2a7b5a3b4403c03b4287be49c1afbc
giuseppe-eustaquio <gseppe222@gmail.com>
2022-09-01 13:47:05 +0800

    adds API functionality for quotes and local storage

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("Hello world!");
+// Show new quote
+
+function newQuote(apiQuotes) {
+  // Pick a random quote from apiQuotes array
+
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
+
+// Get Quotes from API
+
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
+
+  try {
+    const response = await fetch(apiUrl);
+    const apiQuotes = await response.json();
+
+    newQuote(apiQuotes);
+  } catch (error) {
+    // Catch error/s here
+  }
+}
+
+// On Load
+getQuotes();
+
+// Use the localQuotes instead
+// newQuote(localQuotes);
